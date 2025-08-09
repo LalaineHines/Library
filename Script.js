087fe3094e66bfe39c6c437c73ccb27ef0bee53a
LalaineHines <lalainehines@gmail.com>
2025-08-08 18:24:58 -0700

    added checking all the same func, starting on sorting myLibrary

--- Script.js
+++ Script.js
@@ -24,3 +24,26 @@ let myLibrary = [];
 let sortBy = ['id', 'desc'];
 
 // book -constructor- class
+const Book = class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = idCounter;
+        idCounter += 1;
+    }
+};
+
+// Checks if all the values of one type are the same
+const checkAllTheSame = function checkAllTheSame () {
+    let allTheSame = true;
+    myLibrary.reduce((previous, current) => {
+        if (previous === 'init') return current[sortBy[0]];
+        if (current[sortBy[0]] !== previous) allTheSame = false;
+        return current[sortBy[0]];
+    }, 'init');
+    return allTheSame;
+};
+
+// Sort myLibrary
