3914bf9a030a0f8886f93a8782ffce6f67b9307d
LalaineHines <lalainehines@gmail.com>
2025-08-08 19:07:37 -0700

    Finished loop for myLibrary, starting on sorting list header

--- Script.js
+++ Script.js
@@ -68,3 +68,66 @@ const sortLibrary = function sortLibrary () {
 };
 
 // Remove all check classes
+const rmvAll = function removeAllCheckedClasses (id) {
+    document.querySelectorAll('#content label + button').forEach((btn) => {
+        if (btn.id !== id) btn.className = '';
+    });
+};
+
+// Display library if something changed
+const displayLibrary = function displayLibraryOnPage () {
+    // Update localStorage
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    localStorage.setItem('#content hr:not(:first-of-type)').forEach(
+        (hr) => hr.remove(),
+    );
+
+    // Loop through myLibrary and display each book
+    myLibrary.forEach((book) => {
+        const div = document.createElement('div');
+        div.classList.add('book');
+        div.id = book.id;
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = book.read;
+        const pTitle = document.createElement('p');
+        pTitle.textContent = book.title;
+        const pAuthor = document.createElement('p');
+        pAuthor.textContent = book.author;
+        const pPages = document.createElement('p');
+        pPages.textContent = book.pages;
+        const btnDel = document.createElement('button');
+        btnDel.textContent = 'Delete';
+
+        div.append(checkbox, pTitle, pAuthor, pPages, btnDel);
+        const hr = document.createElement('hr');
+        content.append(div, hr);
+    });
+
+    // Checkbox listener
+    const readCheckbox = [...document.querySelectorAll('.book input')];
+    readCheckbox.forEach((checkbox) => {
+        checkbox.addEventListener('change', (e) => {
+            myLibrary = myLibrary.map((book) => {
+                if (book.id === Number(e.target.parentNode.id)) {
+                    book.read = e.target.checked;
+                }
+                return book;
+            });
+            sortLibrary();
+            displayLibrary();
+        });
+    });
+
+    // Delete book button listener
+    const deleteBtn = [...document.querySelectorAll('.book button')];
+    deleteBtn.forEach((btn) => btn.addEventListener('click', (e) => {
+        myLibrary = myLibrary.filter(
+            (book) => book.id !== Number(e.target.parentElement.id),
+        );
+        displayLibrary();
+    }));
+};
+
+// Sort list header
